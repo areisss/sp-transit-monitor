47dddca82026d5c936acd7762f3c8de53b228d6f
Artur Reis <artur.reis@nubank.com.br>
2026-04-02 22:46:43 -0300

    Fix CI: add setuptools package config and Databricks provider
    - pyproject.toml: add build-system and setuptools.packages.find to fix
      flat-layout discovery error in pip install -e
    - terraform: add databricks/databricks provider to required_providers

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.0"
+    }
   }
 
   backend "s3" {
